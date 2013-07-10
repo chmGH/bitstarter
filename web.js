cc062e29ad1453db79be8d5639285c08c73449d8
Chris Miller <marathonman@gmail.com>
2013-07-10 02:11:25 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(100);
+
+fs.readFileSync('index.html',function(err,data){
+  if (err) throw err;
+  buffer.write(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
